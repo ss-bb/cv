b0158e54b72111055b84bdd4dbe68d9115b4a45e
olivier goyon <og@combosolutions.eu>
2020-07-22 18:08:40 +0200

    bn

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+    <v-app id="test" :class="{ 'forced__black': isActive }">
+        <v-app-bar app color="primary" dark v-if="passStep1"></v-app-bar>
+        <v-main>
+            <ComponentIntro @endTimer="endtimer" />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ComponentIntro from "./components/ComponentIntro";
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    name: "App",
+    components: {
+        ComponentIntro
+    },
+    data: () => ({
+        //
+    }),
+    computed: {
+        isActive() {
+            console.log("a", this.currentStep == 1);
+            return this.currentStep == 1;
+        },
+        passStep1() {
+            return this.currentStep >= 2;
+        },
+        currentStep() {
+            return 1;
+        }
+    },
+    methods: {
+        endtimer() {
+            console.log("this is the end");
+        }
+    }
 };
 </script>
+
+<style lang="scss" scoped>
+.forced__black {
+    background-color: black !important;
+}
+</style>
---
+++ src/components/ComponentIntro.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-container>
+        <v-row class="white--text">
+            <v-col>
+                <p></p>
+                <p>
+                    01 AU COMMENCEMENT, L'Homme créa le Web.
+                    <span class="font-weight-bold">le web</span>
+                </p>
+            </v-col>
+        </v-row>
+        <v-row v-if="stepSup1" class="white--text">
+            <v-col class="text-right">
+                <p>02 Le Web était informe et vide, les pixels blanc étaient au-dessus de l’abîme</p>
+            </v-col>
+        </v-row>
+        <v-row v-if="stepSup2" class="white--text">
+            <v-col class="text-center">
+                <p>
+                    03 L'homme dit : « Que la couleurs soit. »
+                    <span
+                        class="blue--text"
+                    >Et la couleurs fut</span>
+                </p>
+            </v-col>
+        </v-row>
+        <v-row v-if="stepSup3" class="white black--text">
+            <v-col>
+                <p>04 L'homme vit que la lumière était bonne, et L'Homme sépara la lumière des ténèbres.</p>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "componentIntro",
+
+    data: () => ({
+        step: 0,
+        timingApparition: 3000
+    }),
+    computed: {
+        stepSup1() {
+            return this.step >= 1;
+        },
+        stepSup2() {
+            return this.step >= 2;
+        },
+        stepSup3() {
+            return this.step >= 3;
+        }
+    },
+    mounted() {
+        setTimeout(() => this.callBackStep(), this.timingApparition);
+    },
+    methods: {
+        callBackStep() {
+            let newStep = this.step + 1;
+            this.step = newStep;
+            if (newStep <= 3) {
+                setTimeout(() => this.callBackStep(), this.timingApparition);
+            } else {
+                this.$emit("endTimer");
+            }
+        }
+    }
+};
+</script>
